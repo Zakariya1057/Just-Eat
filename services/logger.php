47c318a806e187bef1f6ae25f78afd84f90035f3
zakariya1057 <zakaria2011@live.no>
2019-11-17 21:23:02 +0000

    Improve Logger Service

--- services/logger.php
+++ services/logger.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../config/config.php';
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
@@ -9,28 +10,28 @@ use Monolog\Handler\FirePHPHandler;
 global $logger;
 $logger = new Logger('logger');
 
-//2019-08-26
 $date = date('d-m-Y');
 
-if(!file_exists(__DIR__."/../logs/$date")){
-    mkdir(__DIR__."/../logs/$date");
+$city = $config->city;
+$city_directory = __DIR__."/../logs/$city";
+if(!file_exists($city_directory)){
+    mkdir($city_directory);
+}
+
+$logs_directory = "$city_directory/$date";
+if(!file_exists($logs_directory)){
+    mkdir($logs_directory);
 }
 
 $logger->pushHandler(new StreamHandler('php://stdout', Logger::DEBUG));
 
-$logger->pushHandler(new StreamHandler(__DIR__."/../logs/$date/debug.log", Logger::DEBUG));
-
-$logger->pushHandler(new StreamHandler(__DIR__."/../logs/$date/info.log", Logger::INFO));
-
-$logger->pushHandler(new StreamHandler(__DIR__."/../logs/$date/error.log", Logger::ERROR));
-
-$logger->pushHandler(new StreamHandler(__DIR__."/../logs/$date/critical.log", Logger::CRITICAL));
-
-$logger->pushHandler(new StreamHandler(__DIR__."/../logs/$date/alert.log", Logger::ALERT));
-
-$logger->pushHandler(new StreamHandler(__DIR__."/../logs/$date/warning.log", Logger::WARNING));
-
-$logger->pushHandler(new StreamHandler(__DIR__."/../logs/$date/notice.log", Logger::NOTICE));
+$logger->pushHandler(new StreamHandler("$logs_directory/debug.log", Logger::DEBUG));
+$logger->pushHandler(new StreamHandler("$logs_directory/info.log", Logger::INFO));
+$logger->pushHandler(new StreamHandler("$logs_directory/error.log", Logger::ERROR));
+$logger->pushHandler(new StreamHandler("$logs_directory/critical.log", Logger::CRITICAL));
+$logger->pushHandler(new StreamHandler("$logs_directory/alert.log", Logger::ALERT));
+$logger->pushHandler(new StreamHandler("$logs_directory/warning.log", Logger::WARNING));
+$logger->pushHandler(new StreamHandler("$logs_directory/notice.log", Logger::NOTICE));
 
 //allows you to temporarily add a logger with bubbling disabled if you want to override other configured loggers
 $logger->pushHandler(new FirePHPHandler());
