6e29b335f4993bbfa041ee6c8ec641ccd2a6d830
zakariya1057 <zakaria2011@live.no>
2019-11-17 21:22:40 +0000

    Update Email Script

--- services/email.php
+++ services/email.php
@@ -1,12 +1,16 @@
 <?php
 
     require_once  __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../config/config.php';
 
     use PHPMailer\PHPMailer\PHPMailer;
 
-    class email {
 
-        public function send($message){
+    function send_email($message){
+
+            global $config,$logger;
+
+            $email_config = $config->email->configuration;
 
             $mail = new PHPMailer(true);
 
@@ -15,32 +19,32 @@
                 //Server settings
                 $mail->SMTPDebug = 0;                                       // Enable verbose debug output
                 $mail->isSMTP();                                            // Set mailer to use SMTP
-                $mail->Host       = 'secure251.inmotionhosting.com';  // Specify main and backup SMTP servers
-                $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-                $mail->Username   = 'zack@zedbite.com';                     // SMTP username
-                $mail->Password   = 'BW{.iwL0@gHk';                               // SMTP password
-                $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
-                $mail->Port       = 587;                                    // TCP port to connect to
+                $mail->Host       = $email_config->host;                    // Specify main and backup SMTP servers
+                $mail->SMTPAuth   = $email_config->smpt_auth;               // Enable SMTP authentication
+                $mail->Username   = $email_config->username;                // SMTP username
+                $mail->Password   = $email_config->password;                // SMTP password
+                $mail->SMTPSecure = $email_config->smtp_sercure;    
+                $mail->Port       = $email_config->port;                                    // TCP port to connect to
         
                 //Recipients
-                $mail->setFrom('zack@zedbite.com', 'Zedbite');
-                $mail->addAddress('zakaria2011@live.no', 'User');  
+                $mail->setFrom($config->email->from, 'Zedbite');
+                $mail->addAddress($config->email->to, 'User');  
         
                 // Content
                 $mail->isHTML(true);   
-                $mail->addAttachment( __DIR__."/logs/$date/error.log");                                 // Set email format to HTML
+                $mail->addAttachment( __DIR__."/../logs/$config->city/$date/debug.log");                                 // Set email format to HTML
 
-                $mail->Subject = 'Scrape Error';
+                $mail->Subject = $config->email->subject;
                 $mail->Body    = $message;
         
                 $mail->send();
+
+                $logger->debug('Error Email Successfully Sent To '.$config->email->to);
             
         } catch (Exception $e) {
-            echo "Message could not be sent. Mailer Error";
+            $logger->error("Message could not be sent. Mailer Error: ".$e->getMessage());
         }
 
     }
-}
-
 
 ?>
